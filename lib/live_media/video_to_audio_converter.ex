92cc37b17a803ea1519048d2f0ef374341bc2594
claudionts <claudionts@gmail.com>
2024-12-15 19:31:13 -0300

    feat: created file uploaded flow e2e

---
+++ lib/live_media/video_to_audio_converter.ex
@@ -0,0 +1,29 @@
+defmodule LiveMedia.VideoToAudioConverter do
+  @moduledoc """
+  Módulo para converter vídeos MP4 para MP3 usando FFmpeg.
+  """
+
+  @doc """
+  Converte um arquivo MP4 em MP3.
+
+  ## Parâmetros
+
+    - `input_path`: Caminho para o arquivo de vídeo MP4.
+    - `output_path`: Caminho para o arquivo de saída MP3.
+
+  ## Exemplo
+
+      iex> VideoToAudioConverter.convert("video.mp4", "audio.mp3")
+      :ok
+
+  """
+  def convert(input_path, output_path) do
+    case System.cmd("ffmpeg", ["-i", input_path, "-q:a", "0", "-map", "a", output_path]) do
+      {_, 0} ->
+        :ok
+
+      {error_message, _exit_code} ->
+        {:error, error_message}
+    end
+  end
+end
--- lib/live_media_web/endpoint.ex
+++ lib/live_media_web/endpoint.ex
@@ -25,6 +25,11 @@ defmodule LiveMediaWeb.Endpoint do
     gzip: false,
     only: LiveMediaWeb.static_paths()
 
+  # plug Plug.Static,
+  #   at: "/uploads",
+  #   from: :my_app,
+  #   gzip: false
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
